cc2441f053981f49eb2dbc46905b797604ffba30
roshan4545 <60842890+roshan4545@users.noreply.github.com>
2020-12-12 19:45:01 +0530

    Update newsget.py

--- newsget.py
+++ newsget.py
@@ -1,15 +1,31 @@
+
+#beautifulsoup module is used to extract the data out of html and xml files and provides many different parsers for parsing the document.
+#beautifulsoup module should already be installed.
 import bs4
+
+#requests module is used to send HTTP requests and HTTP request returns Response object with all the response data.
+#requests module should already be installed.
 import requests
+
 from datetime import datetime
+
 file = open(r"C:\Users\malipatel roshan\Desktop\default.html","a")
+
 now = datetime.now()
 current_date = now.strftime("%B %d, %Y")
 current_time = now.strftime("%H:%M")
+
+file.write("<head><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'></head>")
 file.write("<h1><b>THE NEWS AT "+current_date+" "+current_time+"</b></h1>")
+
+#here the get method of the requests module is used to send a get request to the specified URL and it returns a Response object
 results=requests.get("https://www.bbc.com/news")
-soup=bs4.BeautifulSoup(results.content,'html.parser', multi_valued_attributes=None)
+ 
+soup=bs4.BeautifulSoup(results.content,'html.parser',multi_valued_attributes=None)
 sample=soup.find(class_="gel-layout gel-layout--no-flex nw-c-top-stories--standard nw-c-top-stories--international")
+
 div_tag=sample.find_all('div')
+
 for div_tag_items in div_tag :
          if 'class' in div_tag_items.attrs:
                 string=div_tag_items['class']
